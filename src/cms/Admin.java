0498a6735e706a17bd3fcc1f46594cb536cff4fe
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2024-03-21 21:26:35 +0000

    Creating admin usertype
    Setting up the first user subclass Admin!

---
+++ src/cms/Admin.java
@@ -0,0 +1,35 @@
+
+package cms;
+
+
+
+
+
+
+public class Admin extends User {
+   
+    //attributes specific to Admin
+    
+        
+
+// Constructor
+      
+    public Admin(String username, String password, String userType) {
+        // Call the constructor of the superclass (User) to initialize inherited attributes
+        super(username, password, userType);
+        
+        // Initialize attributes specific to Admin
+       
+    }
+    
+    
+    
+    
+    
+    
+    
+    //methods specific to Admin
+    
+    
+    
+}
